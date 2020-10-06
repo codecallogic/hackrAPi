bb79e79736d4d51343f697c784e276a48c8070ed
Fabricio <contact@fabricioguardia.com>
2020-10-06 01:16:49 -0700

    create category functionality

--- controllers/category.js
+++ controllers/category.js
@@ -1,5 +1,26 @@
+const Category = require('../models/category')
+const slugify = require('slugify')
+
 exports.create = (req, res) => {
-    //
+    const {name, content} = req.body
+    const slug = slugify(name)
+    const image = {
+        url: `https://via.placeholder.com/350x150.png?text=${process.env.CLIENT_URL}`,
+        key: '123',
+    }
+
+    const category = new Category({name, slug, image})
+    category.postedBy = req.user._id
+
+    category.save((err, data) => {
+        if(err){
+            console.log(err)
+            return res.status(400).json({
+                error: 'Categry create failed'
+            })
+        }
+        res.json(data)
+    })
 }
 
 exports.list = (req, res) => {
