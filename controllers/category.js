dc2b79d32900053b77b384f5da8d32a5ffe28607
Fabricio <contact@fabricioguardia.com>
2020-10-12 20:14:38 -0700

    base64 image

--- controllers/category.js
+++ controllers/category.js
@@ -11,55 +11,113 @@ const S3 = new AWS.S3({
     region: process.env.AWS_REGION
 })
 
-exports.create = (req, res) => {
-    let form = new formidable.IncomingForm()
-    form.parse(req, (err, fields, files) => {
-        console.table((err, fields, files))
-        if(err){
-            return res.status(400).json({
-                error: 'Could not uplaod image'
-            })
-        }
+// exports.create = (req, res) => {
+//     let form = new formidable.IncomingForm()
+//     form.parse(req, (err, fields, files) => {
+//         console.table((err, fields, files))
+//         if(err){
+//             return res.status(400).json({
+//                 error: 'Could not upload image'
+//             })
+//         }
 
-        const {name, content} = fields
-        const {image} = files
+//         const {name, content} = fields
+//         const {image} = files
 
-        const slug = slugify(name)
-        let category = new Category({name, content, slug})
+//         const slug = slugify(name)
+//         let category = new Category({name, content, slug})
         
-        if( image.size > 200000){
+//         if( image.size > 200000){
+//             return res.status(400).json({
+//                 error: 'Image should be less than 2 MB'
+//             })
+//         }
+
+//         const params = {
+//             Bucket: 'codecallogiclab',
+//             Key: `hackr/categories/${uuidv4()}`,
+//             Body: fs.readFileSync(image.path),
+//             // Access control level
+//             ACL: 'public-read',
+//             ContentType: 'image/jpg',
+//         }
+
+//         Category.findOne({name}, (err, category) => {
+//             if(category){
+//                 return res.status(400).json({
+//                     error: 'Cannot have duplicate categories'
+//                 })
+//             }
+//         })
+
+//         S3.upload(params, (err, data) => {
+//             if(err){
+//                 console.log(err)
+//                 return res.status(400).json({
+//                     error: 'Upload to S3 failed'
+//                 })
+//             }
+//             console.log('AWS UPLOAD DATA', data)
+//             category.image.url = data.Location
+//             category.image.key = data.key
+
+//             category.save((err, success) => {
+//                 if(err) {
+//                     console.log(err)
+//                     return res.status(400).json({ error: 'Cannot save category to database'})
+//                 }
+//                 return res.json({success: success})
+//             })
+//         })
+//     })
+// }
+
+exports.create = (req, res) => {
+    const {name, content, image} = req.body
+    // image data
+    const base64Data = new Buffer.from(image.replace(/^data:image\/w+;base64,/, ''), 'base64')
+    console.log(image)
+    console.log(base64Data)
+    const type = image.split(';')[0].split('/')[1]
+
+    const slug = slugify(name)
+    let category = new Category({name, content, slug})
+
+    const params = {
+        Bucket: 'codecallogiclab',
+        Key: `hackr/categories/${uuidv4()}.${type}`,
+        Body: base64Data,
+        // Access control level
+        ACL: 'public-read',
+        ContentEncoding: 'base64',
+        ContentType: `image/${type}`,
+    }
+
+    Category.findOne({name}, (err, category) => {
+        if(category){
             return res.status(400).json({
-                error: 'Image should be less than 2 MB'
+                error: 'Cannot have duplicate categories'
             })
         }
+    })
 
-        const params = {
-            Bucket: 'codecallogiclab',
-            Key: `hackr/categories/${uuidv4()}`,
-            Body: fs.readFileSync(image.path),
-            // Access control level
-            ACL: 'public-read',
-            ContentType: 'image/jpg',
+    S3.upload(params, (err, data) => {
+        if(err){
+            console.log(err)
+            return res.status(400).json({
+                error: 'Upload to S3 failed'
+            })
         }
+        console.log('AWS UPLOAD DATA', data)
+        category.image.url = data.Location
+        category.image.key = data.key
 
-        S3.upload(params, (err, data) => {
-            if(err){
+        category.save((err, success) => {
+            if(err) {
                 console.log(err)
-                return res.status(400).json({
-                    error: 'Upload to S3 failed'
-                })
+                return res.status(400).json({ error: 'Cannot save category to database'})
             }
-            console.log('AWS UPLOAD DATA', data)
-            category.image.url = data.Location
-            category.image.key = data.key
-
-            category.save((err, success) => {
-                if(err) {
-                    console.log(err)
-                    return res.status(400).json({ error: 'Cannot save category to database'})
-                }
-                return res.json(success)
-            })
+            return res.json({success: success})
         })
     })
 }
@@ -87,7 +145,14 @@ exports.create = (req, res) => {
 // }
 
 exports.list = (req, res) => {
-    //
+    Category.find({}, (err, data) => {
+        if(err){
+            return res.status(400).json({
+                error: 'Categories could not load'
+            })
+        }
+        res.json(data)
+    })
 }
 
 exports.read = (req, res) => {
--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const categoryRoutes = require('./routes/category')
 
 // app middlewares
 app.use(morgan('dev'));
-app.use(express.json());
+app.use(express.json({ limit: '5mb', type: 'application/json'}));
 app.use(cors({ origin: process.env.CLIENT_URL}))
 
 app.use('/api', authRoutes)
